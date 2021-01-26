5b37b436d2964ac6aa9c43f39ffa0fb7a70088a3
EllaKoch <ms.kosya555@gmail.com>
2021-01-26 15:29:51 -0500

    added B222

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,8 +1,9 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("hello world, B21");
+        System.out.println("hello world, B222");
         System.out.println("So far so good! yup");
         System.out.println("ready to go back");
+
 
 
     }
