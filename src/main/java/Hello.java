8afef1f7bd32def3fdc0c89a9641d9e365b81321
EllaKoch <ms.kosya555@gmail.com>
2021-01-26 15:37:50 -0500

    added comment on movie 10

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
         System.out.println("So far so good! yup");
         System.out.println("ready to go back");
         System.out.println("trying to conflict");
-
+        //movie on the phone 10 min
 
 
     }
