8eed07c5dc70ca02dac1b1c72f3a4bbcc74d93bb
EllaKoch <ms.kosya555@gmail.com>
2021-01-26 15:26:38 -0500

    added some stuff again

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,7 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello world, B21");
         System.out.println("So far so good! yup");
-
+        System.out.println("ready to go back");
 
 
     }
